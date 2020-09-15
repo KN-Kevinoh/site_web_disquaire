c50c9c4cb2f86b1b3e8ff9f2aa14c3439d6ebfae
KN-Kevinoh <lekankevinoh@gmail.com>
2020-09-15 16:44:00 +0700

    add others gabarit

--- disko_project/store/urls.py
+++ disko_project/store/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.index), # /store may call method index (view) 
-    url(r'^$', views.listing), # /store may call method listing (view)
-    url(r'^(?P<id>[0-9]+)/$', views.details),
-    url(r'^search/$', views.search),
+    url(r'^$', views.listing, name='listing'), # /store may call method listing (view)
+    url(r'^(?P<id>[0-9]+)/$', views.details, name='details'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- disko_project/store/views.py
+++ disko_project/store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from .models import Artist,  Album, Contact, Booking
-from django.template import loader # module loader to load templates
+#from django.template import loader # module loader to load templates
+
 
 # Create your views here.
 
@@ -10,45 +11,60 @@ def index(request):
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
     list_albums = ["<li>{}</li>".format(album.title) for album in albums]
     #message = """<ul>{}</ul>""".format("\n".join(list_albums))
-    template = loader.get_template('strore/index.html')
-    return HttpResponse(template.render(request = request))
+    #template = loader.get_template('strore/index.html')
+    # set gabarit context
+    context = {'albums': albums}
+    return render(request , 'strore/index.html', context)
 
 def listing(request):
     albums = Album.objects.filter(available=True)
-    list_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(list_albums))
-    return HttpResponse(message)
+    #list_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    #message = """<ul>{}</ul>""".format("\n".join(list_albums))
+    context = {'albums': albums}
+    return render(request, 'store/listing.html', context)
 
 def details(request, id):
     album_id = int(id)
     album = Album.objects.get(pk=album_id)
-    artists = " ".join([artist.name for artist in album.artists.all()])
-    message = "Album name is {}. It wrote by {}".format(album.title, artists)
-    return HttpResponse(message) 
+    artists_name = " ".join([artist.name for artist in album.artists.all()])
+    #message = "Album name is {}. It wrote by {}".format(album.title, artists)
+    context = {
+        'album_title': album.title,
+        'album_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture
+    }
+    return render(request, "store/details.html", context) 
 
 def search(request):
     query = request.GET.get('query')
     if not query:
         albums = Album.objects.all()
-        if not albums.exists():
-            message = "Empty albums"
+        #if not albums.exists():
+            #message = "Empty albums"
     else:
         albums = Album.objects.filter(title__icontains=query)
 
         if not albums.exists():
             # get all albums by matching artists name with query
             albums = Album.objects.filter(artists__name__icontains=query)
-        if not albums.exists():
-             message = "No albums found"
+       # if not albums.exists():
+           #  message = "No albums found"
 
-        else:
-            albums = ["<li>{}</li>".format(album.title) for album in albums]
-            message = """
-                    Search results:\n
-                    <ul>
-                        {}
-                    </ul>
-                """.format("<li>{}</li>".join(albums))
+      #  else:
+          #  albums = ["<li>{}</li>".format(album.title) for album in albums]
+          #  message = """
+           #         Search results:\n
+            #        <ul>
+             #           {}
+              #      </ul>
+               # """.format("<li>{}</li>".join(albums))
 
+    title = "Requests results of %s"%query
+
+    context = {
+        'albums': albums,
+        'title':title
+    }
     
-    return HttpResponse(message)
+    return render(request, 'store/search.html', context)
