053c61ede13de843c0e466545fd3f2b62d8668e0
KN-Kevinoh <lekankevinoh@gmail.com>
2020-09-30 19:47:41 +0700

    update settings

--- disko_project/disko_project/settings.py
+++ disko_project/disko_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3wrs18lg+3uf2bpb6nf7a(=@=0-!@f8)apugv4vi6j&&hz3fek'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
